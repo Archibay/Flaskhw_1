8fce0af4e6525fa5d2404c82205781259ac5e237
Artur <archibay16@gmail.com>
2022-10-11 19:43:20 +0300

    still working_2

--- tracks/blog.py
+++ tracks/blog.py
@@ -4,18 +4,14 @@ from flask import (
 from werkzeug.exceptions import abort
 
 # from flaskr.auth import login_required
-from flaskr.db import get_db
+from tracks.db import get_db
 
 bp = Blueprint('blog', __name__)
 
 
 @bp.route('/')
 def index():
-    db = get_db()
-    ind = db.execute(
-        'SELECT * FROM tracks'
-    ).fetchall()
-    return render_template('blog/main.html', ind=ind)
+    return render_template('blog/main.html')
 
 
 @bp.route('/names/')
@@ -32,25 +28,27 @@ def names():
 def tracks():
     db = get_db()
     tr = db.execute(
-        'SELECT COUNT (*) FROM tracks'
-    )
+        'SELECT DISTINCT id FROM tracks'
+    ).fetchall()
+    tr = len(tr)
     return render_template('blog/tracks.html', tr=tr)
 
 
-@bp.route('/tracks/rock')
-def cnt_genre():
+@bp.route('/tracks/<path:genre>')
+def cnt_genre(genre):
     db = get_db()
     cg = db.execute(
-        'SELECT COUNT (id) FROM tracks WHERE genre = rock'
-    )
-    return render_template('blog/names.html', cg=cg)
+        'SELECT DISTINCT id FROM tracks WHERE Genre = ?', (genre,)
+    ).fetchall()
+    cg = len(cg)
+    return render_template('blog/cnt_genre.html', cg=cg)
 
 
 @bp.route('/tracks-sec/')
 def tracks_sec():
     db = get_db()
     ts = db.execute(
-        'SELECT * FROM tracks'
+        'SELECT * FROM tracks ORDER BY id'
     ).fetchall()
     return render_template('blog/tracks_sec.html', ts=ts)
 
@@ -59,6 +57,7 @@ def tracks_sec():
 def ts_statistics():
     db = get_db()
     tss = db.execute(
-        'SELECT * FROM tracks'
+        'SELECT SUM (track_length) FROM tracks'
     ).fetchall()
+
     return render_template('blog/ts_statistics.html', tss=tss)
